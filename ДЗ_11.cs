b90172cf5c8dc63e386a32b7e15a003306775773
Gorgggulia <131769387+Gorgggulia@users.noreply.github.com>
2023-06-02 11:04:47 +0300

    Add files via upload

---
+++ ДЗ_11.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+delegate void MyDel(int m);
+
+class Student
+{
+    public string Name { get; }
+    public List<int> Marks { get; }
+
+    public event MyDel MarkChange;
+
+    public Student(string name)
+    {
+        Name = name;
+        Marks = new List<int>();
+    }
+
+    public void AddMark(int mark)
+    {
+        Marks.Add(mark);
+        MarkChange?.Invoke(mark);
+    }
+}
+
+class Parent
+{
+    public void OnMarkChange(int mark)
+    {
+        Console.WriteLine($"Parent: The student received a mark of {mark}");
+    }
+}
+
+class Accountancy
+{
+    public void PayingFellowship(int mark)
+    {
+        if (mark >= 90)
+        {
+            Console.WriteLine("Accountancy: The student is eligible for a scholarship.");
+        }
+        else
+        {
+            Console.WriteLine("Accountancy: The student is not eligible for a scholarship.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Student student = new Student("John");
+        Parent parent = new Parent();
+        Accountancy accountancy = new Accountancy();
+
+        student.MarkChange += parent.OnMarkChange;
+        student.MarkChange += accountancy.PayingFellowship;
+
+        student.AddMark(80);
+        student.AddMark(95);
+        student.AddMark(88);
+    }
+}
